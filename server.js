3aaf1be518c5e7d8e8602c40b6bef7a5dff0f442
Yvan2Ero <yvankanaj@gmail.com>
2021-09-06 00:25:51 +0100

    Resolution du bug lors de l'actualisation d'une page autre que le /home

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + `/dist/portfolio`));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/portfolio/index.html"));
 });
 app.listen(process.env.PORT);
