14774fdca692fbc2b9a0cfe2d4022a0a8e164511
Yvan2Ero <yvankanaj@gmail.com>
2021-09-05 16:22:20 +0100

    Configuration pour le premier deployement

--- server.js
+++ server.js
@@ -8,5 +8,4 @@ app.use(express.static(__dirname + `/dist/portfolio`));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/portfolio/index.html"));
 });
-
 app.listen(process.env.PORT);
